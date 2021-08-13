65aa252c0333cb4c1d50da827932234ad23472f1
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-08-13 16:00:37 +1000

    Hamburger menu updated
    Now if the max-width of the display is under 690px then the hamburger icon will cover up the space and store it in a block level element.

--- Capstone project/JavaScript/script.js
+++ Capstone project/JavaScript/script.js
@@ -15,3 +15,21 @@ else{
 }
 
 ///hamburger
+
+
+let x= document.querySelector(".sticky-mobile-nav");
+let y = document.querySelector(".container");
+let menuStatus = false;
+x.addEventListener("click", function onMenuClick(){
+   if (menuStatus== false){
+       y.style.display = "none";
+       menuStatus =true;
+   }
+
+   else if (menuStatus == true){
+y.style.display = "block" ;
+y.style.color = "blue" ;
+menuStatus = false;
+
+   }
+})
