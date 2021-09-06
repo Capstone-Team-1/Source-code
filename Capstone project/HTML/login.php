c45bd82a390df171e52a2ceb49f857801b4dc554
Juan Maurente <juanmaurente@gmail.com>
2021-09-06 20:30:36 +1000

    Update login.php

--- Capstone project/HTML/login.php
+++ Capstone project/HTML/login.php
@@ -1,21 +1,20 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login Page</title>
-    <link rel="stylesheet" href="../CSS/style.css">
-    
-    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
-    
-    <style>
-     
-    </style>
 
-<!-- ------- this code was at the top of this page. I (JUAN) commented and pasted here to try another connection
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>login Page</title>
+  <link rel="stylesheet" href="../CSS/style.css">
+
+  <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+
+  <style>
+
+  </style>
+
+  <!-- ------- this code was at the top of this page. I (JUAN) commented and pasted here to try another connection
 write the php opening tag to use
 
 include 'connection.php';
@@ -57,145 +56,126 @@ else{
 }
 ?> -->
 </head>
+
 <body>
-    
- 
-         <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
-          <!--The div underneath contains the google translator link and initial langugage is set to english with en ----->
-<div id="google_translate_element"></div>
- <script type="text/javascript"> 
-   function googleTranslateElementInit() {
-  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
-  }
-</script>
-    
-<header>
- <a href="../Index.php">  <img src="../Images/Citizens Electoral.jfif " alt="ecoa logo" class="logo"></a>  
-        <h1 class="first">Election Commission of  Australia</h1>
-        <div class="sticky-mobile-nav" ><ion-icon name="menu"></ion-icon></div>
-        <div class= "container">
-          <div class="item one">
-                 
-            <a href="../Index.php">Homepage</a> </div>
-              <div class="item two">For candidates </div>
-               <div class="item three"> About election </div>
-                <div class="item four"> Information Centre</div>
-                 <div class="item five ">  <a href="./admin.php"> ECOA</a> </div>
-               <!---  <div class="item six"><ion-icon name="menu"></ion-icon></div>-->
+
+
+  <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
+  <!--The div underneath contains the google translator link and initial langugage is set to english with en ----->
+  <div id="google_translate_element"></div>
+  <script type="text/javascript">
+    function googleTranslateElementInit() {
+      new google.translate.TranslateElement({
+        pageLanguage: 'en'
+      }, 'google_translate_element');
+    }
+  </script>
+
+  <header>
+    <a href="../Index.php"> <img src="../Images/Citizens Electoral.jfif " alt="ecoa logo" class="logo"></a>
+    <h1 class="first">Election Commission of Australia</h1>
+    <div class="sticky-mobile-nav">
+      <ion-icon name="menu"></ion-icon>
+    </div>
+    <div class="container">
+      <div class="item one">
+
+        <a href="../Index.php">Homepage</a>
+      </div>
+      <div class="item two">For candidates </div>
+      <div class="item three"> About election </div>
+      <div class="item four"> Information Centre</div>
+      <div class="item five "> <a href="./admin.php"> ECOA</a> </div>
+      <!---  <div class="item six"><ion-icon name="menu"></ion-icon></div>-->
+    </div>
+  </header>
+
+
+  <section class="banner">
+
+    <div class="banner-container">
+      <div class="banner-items">
+        <h2 class="banner-header">
+          <br> Welcome to ECOA online voting platform
+          <br>Please fill up the form to continue!
+        </h2>
+
+      </div>
+    </div>
+  </section>
+  <div class="section-form">
+    <div class="login">
+      <h2>Election Commission Of Australia </h2>
+      <p id="template"></p>
+
+      <form action="tryloginCITIZEN.php" method="POST" onsubmit="return validate();">
+        <label for="citizenID"></label>
+        <input type="text" placeholder="Citizen's ID" id="vid" name="citizenID" required">
+        <input type="password" placeholder="Password" id="pw" name="citizenPassword" required>
+        <input type="submit" class="button" Value="Login"></input>
+        <br>
+        <label for="check"> Not registered?
+          <br>Click the link below to register yourself
+        </label>
+        <a href="register.php" style="color:red;"> Register Now</a>
+      </form>
+
+
+    </div>
+  </div>
+  <footer>
+
+    <div class="footer-banner" style="text-align: justify;">
+      <h3 style="color: azure; font-size: 1.1em; font-weight: 550px;">Election comission of Australia</h1>
+
+        <div class="footer-details">
+          <p style="font-family: 'Poppins', sans-serif; font-size: 1.1em;">The ECOA acknowledges the Traditional Owners of country throughout Australia and recognises their continuing connection to land, waters, culture and community. <br> We pay our respects to Elders past, present and emerging.</p>
+
+
+          <div class="footer-container" style="margin-top: 50px;">
+            <div class="footer-item one"> Homepage</div>
+            <div class="footer-item two">For candidates </div>
+            <div class="footer-item three"> About election </div>
+            <div class="footer-item four"> Information Centre</div>
+            <div class="footer-item five "> The ECOA </div>
+          </div>
+
+
+
+
         </div>
-    </header>
 
+        <div class="footer-link">
 
-    <section class="banner">
-     
-            <div class="banner-container">
-              <div class="banner-items" >
-                <h2 class="banner-header">
-                    <br> Welcome to ECOA online voting platform
-                    <br>Please fill up the form to continue!
-                </h2>
-               
-                    </div>
+          <div class="footxer-link-one"> <a href="https://www.facebook.com/pages/AEC%20-%20Australian%20Electoral%20Commission/122364357847301/">
+              <ion-icon name="logo-facebook"></ion-icon>
+            </a> </div>
+          <div class="footxer-link-two"> <a href="https://twitter.com/auselectoralcom">
+              <ion-icon name="logo-twitter"></ion-icon>
+            </a> </div>
+          <div class="fooxter-link-three"> <a href="https://www.facebook.com/pages/AEC%20-%20Australian%20Electoral%20Commission/122364357847301/">
+              <ion-icon name="logo-instagram"></ion-icon>
+            </a> </div>
+
         </div>
-</section>
-<div class="section-form">  
-<div class="login">
-     <h2>Election Commission Of Australia </h2>
-    <p id= "template"></p>
-    
-    <form action="tryloginCITIZEN.php" method="POST" onsubmit="return validate();">
-                        <label for="citizenID"></label>
-                        <input type="text" placeholder="Citizen's ID" name="citizenID" required">
-                        <input type="password" placeholder="Password" name="citizenPassword" required>
-                        <input type="submit" class="button" Value="Login"></input>
-                    </form>
 
 
 
 
-    <form action="" method="POST" class = "siginForm" onsubmit="return validate();">
-<label for="vid"></label> 
-<input type="text" name="votersId" id="vid" placeholder="Voters ID" >
-<br>
-<br>
-<label for="pw"></label><input type="password" name="passWord" id="pw" placeholder="Password"> <!--The name parameter is the one responsible in connection with php--->
-<br>
-<br>
-<br>
-<!-- <label for="pw">Confirm Password</label>
-<br>
-<br>
-<input type="password" name="cpassWord" id="pw" value="<?php echo $_POST['cpassWord']; ?>">
-<br>
-<br> -->
-<input type="submit" name ="login" class="button">
 
-<br>
-<br>
-<label for="check">
-    Not registered?
-    <br>
-    <br>
-    Click the link below to register yourself
-</label>
-<br>
-<br>
-<a href="register.php" style="color:red;"> Register Now</a>
-
-</form>
+    </div>
 
 
-<div></div>
-
-</div>
-</div>
-<footer>
-
-<div class="footer-banner" style="text-align: justify;" >
-    <h3 style="color: azure; font-size: 1.1em; font-weight: 550px;">Election comission of Australia</h1>
-
-<div class= "footer-details">
-    <p style="font-family: 'Poppins', sans-serif; font-size: 1.1em;">The ECOA acknowledges the Traditional Owners of country throughout Australia and recognises their continuing connection to land, waters, culture and community. <br> We pay our respects to Elders past, present and emerging.</p>   
-
-
-     <div class= "footer-container" style="margin-top: 50px;"> 
-             <div class="footer-item one"> Homepage</div>
-              <div class="footer-item two">For candidates </div>
-               <div class="footer-item three"> About election </div>
-                <div class="footer-item four"> Information Centre</div>
-                 <div class ="footer-item five "> The ECOA </div>
-                  </div>
+  </footer>
 
 
 
 
-</div>
-
-       <div class="footer-link">
-
-                    <div class="footxer-link-one">   <a href="https://www.facebook.com/pages/AEC%20-%20Australian%20Electoral%20Commission/122364357847301/"> <ion-icon name="logo-facebook"></ion-icon></a>  </div>
-                    <div class="footxer-link-two">   <a href="https://twitter.com/auselectoralcom"> <ion-icon name="logo-twitter"></ion-icon></a>   </div>
-                      <div class="fooxter-link-three">   <a href="https://www.facebook.com/pages/AEC%20-%20Australian%20Electoral%20Commission/122364357847301/"> <ion-icon name="logo-instagram"></ion-icon></a>  </div>
-                       
-                </div> 
-         
-
-
-
-
-</div>
-
-       
- </footer>
-    
-
-
-
-<script src="../JavaScript/script.js"></script>
+  <script src="../JavaScript/script.js"></script>
   <!--This is the script towards the google translator api script file-->
-    <script type="text/javascript" 
- src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit">
-</script>
+  <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit">
+  </script>
 
 </body>
+
 </html>
