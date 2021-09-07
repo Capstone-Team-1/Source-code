a76fc43a88e4173ded68d6b5741f77030c2fe2dd
Juan Maurente <juanmaurente@gmail.com>
2021-09-07 11:02:26 +1000

    Update register.php

--- Capstone project/register.php
+++ Capstone project/register.php
@@ -102,7 +102,7 @@ if (isset($_POST["signup"])) {
 
       </div>
       <div class="item four"> Information Centre</div>
-      <div class="item five "> <a href="./admin.php"> ECOA</a> </div>
+      <div class="item five "> <a href="./eyadminLogin.php"> ECOA</a> </div>
 
     </div>
     </header>
