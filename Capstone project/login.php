2c26c28b3392d31a76b098a55e330fb7ce26e34e
Juan Maurente <juanmaurente@gmail.com>
2021-09-07 10:32:47 +1000

    Update login.php
    login links FIX

--- Capstone project/login.php
+++ Capstone project/login.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>login Page</title>
-  <link rel="stylesheet" href="../CSS/style.css">
+  <link rel="stylesheet" href="./CSS/style.css">
 
   <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 
@@ -72,7 +72,7 @@ else{
   </script>
 
   <header>
-    <a href="../Index.php"> <img src="../Images/Citizens Electoral.jfif " alt="ecoa logo" class="logo"></a>
+    <a href="./Index.php"> <img src="./Images/Citizens Electoral.jfif " alt="ecoa logo" class="logo"></a>
     <h1 class="first">Election Commission of Australia</h1>
     <div class="sticky-mobile-nav">
       <ion-icon name="menu"></ion-icon>
@@ -80,7 +80,7 @@ else{
     <div class="container">
       <div class="item one">
 
-        <a href="../Index.php">Homepage</a>
+        <a href="./Index.php">Homepage</a>
       </div>
       <div class="item two">For candidates </div>
       <div class="item three"> About election </div>
@@ -171,7 +171,7 @@ else{
 
 
 
-  <script src="../JavaScript/script.js"></script>
+  <script src="./JavaScript/script.js"></script>
   <!--This is the script towards the google translator api script file-->
   <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit">
   </script>
