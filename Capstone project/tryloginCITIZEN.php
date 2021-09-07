a1b2ea8530c01494e4f171718ec05865b503b71d
Juan Maurente <juanmaurente@gmail.com>
2021-09-07 10:15:12 +1000

    Create tryloginCITIZEN.php

---
+++ Capstone project/tryloginCITIZEN.php
@@ -0,0 +1,38 @@
+<?php
+
+$host="localhost"; // Host name
+$username="root"; // Mysql username
+$password=""; // Mysql password
+$db_name="voting"; // Database name
+$tbl_name="citizen"; // Table name
+// Connect to server and select databse.
+
+$conn = mysqli_connect("$host", "$username", "$password")or die("cannot connect");
+
+mysqli_select_db($conn, "$db_name")or die("cannot select DB");
+// username and password sent from form
+$myuser=$_POST['citizenID'];
+$mypassword=$_POST['citizenPassword'];
+// To protect MySQL injection (more detail about MySQL injection)
+$myuser = stripslashes($myuser);
+$mypassword = stripslashes($mypassword);
+$myuser = mysqli_real_escape_string($conn, $myuser);
+$mypassword = mysqli_real_escape_string($conn, md5($mypassword));
+$sql="SELECT * FROM citizen WHERE citizenID ='$myuser' and citizenPassword ='$mypassword'";
+$result=mysqli_query($conn, $sql);
+// Mysql_num_row is counting table row
+$count=mysqli_num_rows($result);
+
+// If result matched $myusername and $mypassword, table row must be 1 row
+if($count==1){
+
+// Register $myusername, $mypassword and redirect to file "login_success.php"
+/* session_start();
+session_register("myuser");
+session_register("mypassword");  */
+header("location: candidate.php");
+}
+else {
+echo "Wrong Username or Password";
+}
+?>
