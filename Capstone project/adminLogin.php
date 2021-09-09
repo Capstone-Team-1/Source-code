fa013fa45bc8bf42440990f2797f85d92dcae287
Juan Maurente <juanmaurente@gmail.com>
2021-09-09 15:25:09 +1000

    Update adminLogin.php
    New navbar implemented and redesign of forms

--- Capstone project/adminLogin.php
+++ Capstone project/adminLogin.php
@@ -55,136 +55,114 @@ if (isset($_POST['login'])) {
     }
   </script>
   <header>
-    <a href="./Index.php"> <img src="./Images/Citizens Electoral.jfif " alt="ecoa logo" class="logo"></a>
-    <h1 class="first">Election Commission of Australia</h1>
-    <div class="sticky-mobile-nav">
-      <ion-icon name="menu"></ion-icon>
-    </div>
-    <div class="container">
-                
-                <div class="item one">
-                    <div class="dropdown"> For Voters
-                    <div class="dropdown-content">
-                        <a href="./voterLogin.php">Voter Login</a>
-                        <a href="./register.php"> Registration</a>
-                    </div>
-                    </div>
-                </div>
-                <!-- <div class="item two"> Candidates </div> -->
-                <div class="item three"> <a href="./about.php"> About election</a>
 
-                </div>
-                <div class="item four"> Information Centre</div>
-                <div class="item five "> <a href="./adminLogin.php"> ECOA</a> </div>
-                <div class="item zero"> <a href="./Index.php"> Home</a> </div>
-                </div>
- 
+    <nav>
+      <input type="checkbox" id="check">
+      <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+      </label>
+      <div class="ecoa-logo">
+        <img src="./Images/Citizens Electoral.jfif " alt="ecoa logo" class="logo">
+        <h1 class="organization-name">Electoral Commission of Australia</h1>
+      </div>
+      <ul>
+        <li><a href="./Index.php">Home</a></li>
+        <li><a href="">Voters</a>
+          <ul class="dropdown">
+            <li><a href="./voterLogin.php">Voter Login</a></li>
+            <li><a href="./register.php">Registration</a></li>
+          </ul>
+        </li>
+        <li><a href="./about.php">About Elections</a></li>
+        <li><a href="">Instructive Video</a></li>
+        <li><a href="">ECOA</a>
+          <ul class="dropdown">
+            <li><a href="./adminLogin.php">Staff Login</a></li>
+            <li><a href="./contactus.php">Contact Us</a></li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
   </header>
 
 
   <section class="banner">
 
     <div class="banner-container">
-      <div class="banner-items">
-        <h2 class="banner-header">
-          <br> Admin Login
-          <br>Please fill up the form to continue!
-        </h2>
+      <div class="banner-items" style="text-align: center; font-size: 20px;">
+        <br>
+        <br>
+        <h2>Staff Login Page</h2>
 
       </div>
     </div>
+  </section>
 
-    <div class="section-form">
-      <div class="login">
-        <h2>ECOA Staff Login</h2>
-        <p id="template"></p>
+  <div class="section-form">
+    <div class="login">
+      <h3>Please enter your credentials </h3>
+      <p id="template"></p>
+      <br>
 
-        <form action="" method="POST" onsubmit="return validate();">
-
-          <br>
-          <br>
-          <input type="placeholder" name="vid" id="vid" placeholder="Admin ID">
-
-          <br>
-          <br>
-          <input type="password" name="pw" id="pw" placeholder="Password">
-          <br>
-          <br>
-          <input type="submit" name="login" class="button">
-
-          <br>
-          <br>
-
-          <label for="check">
-            Are you a voter?
-            <br>
-            <br>
-            Click the link below to be directed towards voters page!!
-          </label>
-          <br>
-          <br>
-          <a href="./login.php" style="color:red;"> Voters Login</a>
-
-        </form>
-
-
-
-        <div></div>
-
-      </div>
+      <form action="" method="POST" onsubmit="return validate();">
+        <input type="text" name="vid" id="vid" placeholder="Admin ID"><br>
+        <input type="password" name="pw" id="pw" placeholder="Password"><br>
+        <input type="submit" name="login" class="button">
+      </form>
     </div>
-    <footer>
+  </div>
+  <footer>
 
-      <div class="footer-banner">
-        <h3 style="color: azure; font-size: 1.1em; font-weight: 550px;">Election comission of Australia</h1>
+    <div class="footer-banner">
+      <h3 style="color: azure; font-size: 1.1em; font-weight: 550px;">Election comission of Australia</h1>
 
-          <div class="footer-details">
-            <p style="font-family: 'Poppins', sans-serif; font-size: 1.1em; justify-content:center">The ECOA acknowledges the Traditional Owners of country throughout Australia and recognises their continuing connection to land, waters, culture and community. <br> We pay our respects to Elders past, present and emerging.</p>
-
-
-            <div class="footer-container" style="margin-top: 50px;">
-              <div class="footer-item one"> <a href="./result.php"> For Voters</a> </div>
-              <div class="footer-item two">For candidates </div>
-              <div class="footer-item three"> About election </div>
-              <div class="footer-item four"> Information Centre</div>
-              <div class="footer-item five "> The ECOA </div>
-            </div>
+        <div class="footer-details">
+          <p style="font-family: 'Poppins', sans-serif; font-size: 1.1em; justify-content:center">The ECOA acknowledges the Traditional Owners of country throughout Australia and recognises their continuing connection to land, waters, culture and community. <br> We pay our respects to Elders past, present and emerging.</p>
 
 
-
-
-          </div>
-
-          <div class="footer-link">
-
-            <div class="footxer-link-one">
-              <ion-icon name="logo-facebook"></ion-icon>
-            </div>
-            <div class="footxer-link-two">
-              <ion-icon name="logo-twitter"></ion-icon>
-            </div>
-            <div class="fooxter-link-three">
-              <ion-icon name="logo-instagram"></ion-icon>
-            </div>
-
+          <div class="footer-container" style="margin-top: 50px;">
+            <div class="footer-item one"> <a href="./result.php"> For Voters</a> </div>
+            <div class="footer-item two">For candidates </div>
+            <div class="footer-item three"> About election </div>
+            <div class="footer-item four"> Information Centre</div>
+            <div class="footer-item five "> The ECOA </div>
           </div>
 
 
 
 
+        </div>
 
-      </div>
+        <div class="footer-link">
+
+          <div class="footxer-link-one">
+            <ion-icon name="logo-facebook"></ion-icon>
+          </div>
+          <div class="footxer-link-two">
+            <ion-icon name="logo-twitter"></ion-icon>
+          </div>
+          <div class="fooxter-link-three">
+            <ion-icon name="logo-instagram"></ion-icon>
+          </div>
+
+        </div>
 
 
-    </footer>
+
+
+
+    </div>
+
+
+  </footer>
 
 
 
 
-    <script src="../JavaScript/script.js"></script>
-    <!--This is the script towards the google translator api script file-->
-    <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit">
-    </script>
+  <script src="../JavaScript/script.js"></script>
+  <!--This is the script towards the google translator api script file-->
+  <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit">
+  </script>
 
 </body>
 
