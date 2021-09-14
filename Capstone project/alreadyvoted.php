2b10332cb59e528e34a6ddca1c563efdbdc64b15
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-09-14 12:18:52 +1000

    link fix
    fix

--- Capstone project/alreadyvoted.php
+++ Capstone project/alreadyvoted.php
@@ -20,12 +20,7 @@ if(empty($_SESSION['citizenID']))
     
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
     
-    <style>
-     a:hover{
-         text-decoration: underline;
-         transition: all 0.3s ease;
-     }
-    </style>
+    
 </head>
 <body>
     
