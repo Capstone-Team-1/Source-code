9c7ef789122902711d0f4913b979da1cda946df4
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-09-14 11:35:39 +1000

    comments in JS
    Comments in JS in case of any confusion to debug it later on

--- Capstone project/candidate.php
+++ Capstone project/candidate.php
@@ -73,17 +73,17 @@ if (isset($_POST["submit_vote"])) {
 
         <form action="" method="post">
       <div class="modal-body" id="popup">
-            <p>Are you sure you want to give vote to this candidate?</p>
-              <select id="final-list" name="vote" value="<?php echo $_POST["vote"]; ?>"> 
-  
-</select>
+            <p>Are you sure you want to give your vote to <strong id="candidate_ID"> </strong>  ?</p>
+    
             <p>This action is irreversible.</p>
             <p>Once you click '<b>Yes</b>', you cannot change your vote.</p>
             
                         <!-- <input type="hidden" name="candidate_id" id="candidate_id">
                         <input type="hidden" name="voter_id" id="voter_id"> -->
     <!-- <input list="candidates" name="candidates-List"> -->
-
+  <select id="final-list" name="vote" value="<?php echo $_POST["vote"]; ?>" style="display: none;"> 
+  
+</select>
             <button type="button" class="btnNo">No</button>
             <button type="submit" name="submit_vote" class="submit">Yes</button>
        </div>
@@ -211,24 +211,25 @@ if (isset($_POST["submit_vote"])) {
 </script>
 <script >
 
-  const btnOne = document.querySelector('.buttonOne');
-  const btnTwo = document.querySelector('.buttonTwo');
-  const btnThree = document.querySelector('.buttonThree');
-  const b = document.querySelector('#popup');
-  const c = document.querySelector('.btnNo');
-  const d = document.querySelector('.blur');
-const selected_list = document.querySelector('#final-list');
-
+  const btnOne = document.querySelector('.buttonOne'); //this selects the first button among the candidate i.e. Malcom
+  const btnTwo = document.querySelector('.buttonTwo'); //this selects the second button mong the candiate i.e. Anothony
+  const btnThree = document.querySelector('.buttonThree'); // this one selects scott 
+  const b = document.querySelector('#popup'); //this is the pop up which appears when you click on any of the candidates
+  const c = document.querySelector('.btnNo'); //this button is the no bbutton which is removingb the blur background if the user does not wants to vote
+  const d = document.querySelector('.blur'); // this is use to select the background blur class and add and remove it
+const selected_list = document.querySelector('#final-list'); //this will be the list that we add dynamically
+const candidate = document.querySelector('#candidate_ID'); //this will be the name of our candidate as the button click
+//we will add event listener to every buttons.
 btnOne.addEventListener('click', ()=>{
-b.classList.add('modal-body-click');
-d.classList.add('bg-blur');
-const newElement = document.createElement('option');
-const newNode = document.createTextNode(1);
-newElement.setAttribute('name', "vote");
+b.classList.add('modal-body-click'); //we are adding the class 
+d.classList.add('bg-blur'); //adding the background blur class
+const newElement = document.createElement('option'); //creating a element to select the candidate
+const newNode = document.createTextNode(1);  //this is the value that is passed to db
+newElement.setAttribute('name', "vote"); //setting the attribute hence PHP can understand
+candidate.innerHTML = "Mr. Malcomm Turnbull"; //appending the name as per the button selection
 
-
-newElement.appendChild(newNode);
-selected_list.appendChild(newElement);
+newElement.appendChild(newNode); //appening the text node that we created in our element
+selected_list.appendChild(newElement); //and appending the new element into our select list that we have slected here using JS
 
 })
 btnTwo.addEventListener('click', ()=>{
@@ -237,6 +238,8 @@ d.classList.add('bg-blur');
 const newElement = document.createElement('option');
 const newNode = document.createTextNode(2);
 newElement.setAttribute('value', "vote");
+
+candidate.innerHTML = "Mr. Anthony Albanese";
 newElement.appendChild(newNode);
 selected_list.appendChild(newElement);
 
@@ -247,6 +250,7 @@ d.classList.add('bg-blur');
 const newElement = document.createElement('option');
 const newNode = document.createTextNode(3);
 newElement.setAttribute('value', "vote");
+candidate.innerHTML = "Mr. Scott Morrison";
 newElement.appendChild(newNode);
 selected_list.appendChild(newElement);
 })
@@ -254,7 +258,7 @@ selected_list.appendChild(newElement);
 
 
 
-
+//this is the one which is removing the background
 c.addEventListener('click', ()=>{
   b.classList.remove('modal-body-click');
   d.classList.remove('bg-blur');
