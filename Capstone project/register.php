02ad6313fe5d0bde7be58d73c8d0e36569d2d505
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-09-23 10:26:46 +1000

    date of birth validation

--- Capstone project/register.php
+++ Capstone project/register.php
@@ -200,10 +200,63 @@ if (isset($_POST["signup"])) { //once the button is click and if the value isnt 
     </div>
   </footer>
 
-      <script src="./JavaScript/register.js"></script>
+      <script src="./JavaScript/regster.js"></script>
       <!--This is the script towards the google translator api script file-->
       <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit">
       </script>
+
+      <script>
+      let citizenShipId= document.getElementById('cid');
+let firstName = document.getElementById('fname');
+let lastName= document.getElementById('lname');
+let phoneNum= document.getElementById('phonenum');
+let dateofBirth= document.getElementById('Dateof');
+let citizenEmail= document.getElementById('cEmail');
+let citizenAddress= document.getElementById('caddress');
+let citizenCity= document.getElementById('city');
+let citizenPassword= document.getElementById('Citizenpw');
+let citizenCPassword= document.getElementById('cpw');
+let displayMessage = document.getElementById('template');
+let dob = new Date(dateofBirth.value);
+ let month_diff = Date.now() - dob.getTime();  
+      
+    //convert the calculated difference in date format  
+    let age_dt = new Date(month_diff);   
+      
+    //extract year from date      
+    let year = age_dt.getUTCFullYear();  
+      
+    //now calculate the age of the user  
+    let age = Math.abs(year - 1970); 
+    console.log(age);
+
+function validateRegister(){
+
+
+
+
+if (citizenShipId.value == "" || firstName.value =="" || lastName.value =="" || phoneNum.value=="" || dateofBirth.value =="" || citizenEmail.value=="" || citizenAddress.value == "" || citizenCity.value =="" || citizenPassword.value==""){
+displayMessage.innerHTML= "Please fill in all the required details";
+    
+
+return false;
+
+}
+else if(age<18)
+{
+    displayMessage.innerHTML= "You are not eligible to vote as you are under 18";
+    return false;
+}
+else{
+    return true;
+}
+
+
+}
+
+
+
+ </script>
 
   </body>
 
